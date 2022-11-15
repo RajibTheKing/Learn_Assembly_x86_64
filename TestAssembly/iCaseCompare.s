8ce935951d58c161ee2f4ecf312a0b6c34a8587b
Rajib Chandra Das <rajib.das@smartclip.tv>
2022-11-15 17:43:42 +0100

    add informative comment

--- TestAssembly/iCaseCompare.s
+++ TestAssembly/iCaseCompare.s
@@ -15,8 +15,8 @@ head_loop:
 
     movdqu      (%rax),     %xmm10              # move first 64 bit of str1 to xmm10 register(SSE)
     movdqu      (%rsi),     %xmm11              # move first 64 bit of str2 to xmm11 register(SSE)
-    add         $16,         %rax               # skip the pointer by 8 bytes
-    add         $16,         %rsi               # skip the pointer by 8 bytes
+    add         $16,         %rax               # skip the pointer by 16 bytes
+    add         $16,         %rsi               # skip the pointer by 16 bytes
 
     /* Converting str1 to Lower */
     pcmpistrm   $0x44,      %xmm10, %xmm12      # MASK: Compare characters is in range A <= c <= Z and store result bytes in xmm0
@@ -26,14 +26,14 @@ head_loop:
     /* Converting str2 to Lower */
     pcmpistrm   $0x44,      %xmm11, %xmm12      # MASK: Compare characters is in range A <= c <= Z and store result bytes in xmm0
     pand        %xmm13,     %xmm0               # TO_ADD: CHARACTER_DIFF & MASK
-    paddb       %xmm0,      %xmm11              # LOWER: TO_ADD + COPY --> xmm15
+    paddb       %xmm0,      %xmm11              # LOWER: TO_ADD + COPY --> xmm11
 
 
 
     /* Check str1 and str2 are identical or not */
     pcmpistrm   $0x18,      %xmm10, %xmm11      # compare two sse register completely equal or not
-    movq        %xmm0,      %r12                 # move the result of xmm0 register to r8 register (temp) to perform cmp instruction
-    sub         $0x00,      %r12                 # check the output after pcmpistrm comparison
+    movq        %xmm0,      %r12                # move the result of xmm0 register to r12 register (temp) to perform cmp instruction
+    sub         $0x00,      %r12                # check the output after pcmpistrm comparison
     jz          head_loop
     jnz         return_result_mismatch
 
@@ -45,8 +45,10 @@ explicit_length_compare:
     movdqu      (%rax),     %xmm10              # move first 64 bit of str1 to xmm10 register(SSE)
     movdqu      (%rsi),     %xmm11              # move first 64 bit of str2 to xmm11 register(SSE)
 
-    push %rax
-    movq $2, %rax
+
+    push %rax                                   # Saving %rax in the Stack, because %rax,%rdx register is used for exlplicit length compare
+    movq $2, %rax                               # The length of %xmm12=0x5A41 is stored in %rax
+                                                # The length of %xmm10 is stored in %rdx, pcmpestrm is ready to execute
 
     pcmpestrm   $0x44,      %xmm10, %xmm12
     pand        %xmm13,     %xmm0
@@ -58,8 +60,11 @@ explicit_length_compare:
 
     movq %rdx, %rax
     pcmpestrm   $0x18,      %xmm10, %xmm11      # compare two sse register completely equal or not
-    movq        %xmm0,      %r12                # move the result of xmm0 register to r8 register (temp) to perform cmp instruction
+    movq        %xmm0,      %r12                # move the result of xmm0 register to r12 register (temp) to perform cmp instruction
+
+    /* Restore the value of %rax from the stack */
     pop %rax
+
     sub         $0x00,      %r12
     jz          return_result_match
     jnz         return_result_mismatch
@@ -71,3 +76,19 @@ return_result_mismatch:
 return_result_match:
     movq        $0x00,      %rax
     ret
+
+/*
+To compare two <=16 bytes parallely there can be two ways
+    - pcmpistrm/pcmpestrm (16 byte comparisons, can be implicit or explicit length)
+    - subps (128 bit subtraction, can be done between two %xmm/%ymm registers)
+
+After research, I found several reasons why subps instruction cannot be feasible to use
+    - SUBPS... it doesn't set the Condition FLAG, After operation %xmm register need to be checked manually
+    - there is no standard instruction available that checks %xmm/%ymm register is empty/zero
+            source: https://stackoverflow.com/questions/42317528/faster-way-to-test-if-xmm-ymm-register-is-zero
+    - SUBPS... while explicit length, bytes might need to be shifted logically left/right within SSE registers.
+        pslldq only works with immediate values.. which means... 
+        pslldq    $13,   %xmm10     ///< this statement is valid
+        pslldq    %xmm0, %xmm10     ///< this statement is invalid
+            source: https://stackoverflow.com/questions/74435943/how-to-perform-logical-shiftpslldq-within-128-bit-sse-registers-in-gas-assembl
+*/
